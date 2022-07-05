a893538b793fbaa684acda589918fe03e284fbbb
Tatiana Gandra <tatianagandra78@gmail.com>
2022-07-05 16:47:54 -0300

    docs: swagger documentation for the tables controllers fix

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,21 +1,61 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { CreateTableDto } from './dto/create-table.dto.ts';
-import { TableService } from './table.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { TablesService } from './tables.service';
+import { CreateTableDto } from './dto/create-table.dto';
+import { UpdateTableDto } from './dto/update-table.dto';
+import { Table } from './entities/table.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@ApiTags('table')
-@Controller('table')
-export class TableController {
-  constructor(private tableService: TableService) {}
-
-  @Get()
-  findAll() {
-    return this.tableService.findAll();
-  }
+@ApiTags('tables')
+@Controller('tables')
+export class TablesController {
+  constructor(private readonly tablesService: TablesService) {}
 
   @Post()
-  create(@Body() createTableDto: CreateTableDto) {
-    return this.tableService.create(createTableDto);
+  @ApiOperation({
+    summary: 'Cria uma nova mesa',
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
+    return this.tablesService.create(dto);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Lista todas as mesas',
+  })
+  findAll(): Promise<Table[]> {
+    return this.tablesService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Lista uma mesa',
+  })
+  findOne(@Param('id') id: string): Promise<Table> {
+    return this.tablesService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualiza uma mesa',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateTableDto): Promise<Table> {
+    return this.tablesService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({
+    summary: 'Exclui uma mesa',
+  })
+  remove(@Param('id') id: string) {
+    return this.tablesService.remove(id);
   }
 }
